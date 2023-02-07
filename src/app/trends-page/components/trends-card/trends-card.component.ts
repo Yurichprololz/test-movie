2eba6d4d4fced05a6c50537052568cd1efc62442
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2023-02-07 21:38:30 +0600

    feat: process card trend

--- src/app/trends-page/components/trends-card/trends-card.component.ts
+++ src/app/trends-page/components/trends-card/trends-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Movie } from 'src/app/shared/model/movie.model';
 
 @Component({
   selector: 'app-trends-card',
   templateUrl: './trends-card.component.html',
-  styleUrls: ['./trends-card.component.scss']
+  styleUrls: ['./trends-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TrendsCardComponent {
-
+  @Input() movie!: Movie
 }
--- src/app/trends-page/page/trends-page/trends-page.component.ts
+++ src/app/trends-page/page/trends-page/trends-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService } from 'src/app/core/services/api.service';
+import { Movie } from 'src/app/shared/model/movie.model';
 
 @Component({
   selector: 'app-trends-page',
   templateUrl: './trends-page.component.html',
-  styleUrls: ['./trends-page.component.scss']
+  styleUrls: ['./trends-page.component.scss'],
 })
-export class TrendsPageComponent {
+export class TrendsPageComponent implements OnInit {
+  movies: Movie[] = [];
+  constructor(private api: APIService) {}
 
+  ngOnInit(): void {
+    this.movies = this.api.getMovies();
+  }
 }
